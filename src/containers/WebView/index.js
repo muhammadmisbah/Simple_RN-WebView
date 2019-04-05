940f719576f3f482e932964025e747a043581bc1
Muhammad Misbah <muhammadmisbah107@gmail.com>
2019-04-05 06:51:12 +0500

    init

--- src/containers/WebView/index.js
+++ src/containers/WebView/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Platform, KeyboardAvoidingView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
+const JS = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);`
+
 export default class Web extends Component {
 
     static navigationOptions = Platform.OS === "ios" ? {} : {
@@ -10,21 +12,11 @@ export default class Web extends Component {
 
     render() {
         return (
-            Platform.OS === "ios" ?
-                <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-                    <WebView
-                        style={{ flex: 1 }}
-                        originWhitelist={['*']}
-                        source={{ uri: 'https://resto1.code509.com/' }}
-                    />
-                </KeyboardAvoidingView>
-                :
-                <WebView
-                    style={{ flex: 1 }}
-                    scalesPageToFit={false}
-                    originWhitelist={['*']}
-                    source={{ uri: 'https://resto1.code509.com/' }}
-                />
+            <WebView
+                style={{ flex: 1 }}
+                originWhitelist={['*']}
+                source={{ uri: 'https://resto1.code509.com/' }}
+            />
         );
     }
 }
